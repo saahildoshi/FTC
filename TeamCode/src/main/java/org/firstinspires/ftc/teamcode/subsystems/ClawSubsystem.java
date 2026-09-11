7980478d2343800151337d3e776b00bdbc763009
bwsayers-ui <bwsayers@oconeeschools.org>
2026-09-11 08:42:09 -0400

    Use fixed competition claw positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ClawSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ClawSubsystem.java
@@ -2,13 +2,14 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.config.LiftConstants;
+import org.firstinspires.ftc.teamcode.config.RobotConstants;
 import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
 
 /**
  * ACTIVE CLAW / ARM SERVO SUBSYSTEM
  *
- * Servo positions come from LiftConstants so they can be tuned live in FTC Dashboard.
+ * Competition claw positions come from RobotConstants so TeleOp and autonomous
+ * always use the same fixed home/closed and open positions.
  */
 public final class ClawSubsystem {
     private final Servo claw;
@@ -18,11 +19,11 @@ public final class ClawSubsystem {
     }
 
     public void open() {
-        claw.setPosition(LiftConstants.CLAW_OPEN_POSITION);
+        claw.setPosition(RobotConstants.Claw.OPEN_POSITION);
     }
 
     public void close() {
-        claw.setPosition(LiftConstants.CLAW_CLOSED_POSITION);
+        claw.setPosition(RobotConstants.Claw.CLOSED_POSITION);
     }
 
     public double getPosition() {
